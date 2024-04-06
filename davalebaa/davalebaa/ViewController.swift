68fff90ad70a46c5e041f6011ef147726b9c5046
Nini Bardavelidze <ninibardavelidze@gmail.com>
2024-04-06 04:55:33 +0400

    Assigments

---
+++ davalebaa/davalebaa/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  davalebaa
+//
+//  Created by Nini Bardavelidze on 06.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var coffeeName: UILabel!
+    @IBOutlet weak var ingridients: UITextField!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var peopleRating: UITextField!
+    @IBOutlet weak var favorites: UIImageView!
+    @IBOutlet weak var discriptionLabel: UILabel!
+    @IBOutlet weak var smallSize: UIButton!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var mediumSize: UIButton!
+    @IBOutlet weak var bigSize: UIButton!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var star: UIImageView!
+    @IBOutlet weak var discriptionText: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
